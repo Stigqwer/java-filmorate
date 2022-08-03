34a528f6a8d537d1e739bf728d03d97090e713d1
Stigqwer <sokolkola1280@gmail.com>
2022-08-04 01:20:17 +0300

    Внесены доработки по замечаниям2

--- src/main/java/ru/yandex/practicum/filmorate/controller/Controller.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/Controller.java
@@ -1,12 +1,14 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@RestController
 public abstract class Controller<T> {
     protected final Map<Integer, T> data = new HashMap<>();
 
